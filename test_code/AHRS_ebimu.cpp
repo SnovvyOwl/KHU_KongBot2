6230bceeb0253591e7cee042fd3dd545dd31c59a
kmh8667 <kmh8667@naver.com>
2020-03-14 22:10:19 +0900

    testcode Upload

--- test_code/AHRS_ebimu.cpp
+++ test_code/AHRS_ebimu.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
+
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 using namespace std;
 int main()
 {
-    cout<<"Hello"<<endl;
-    cout<<"GitTEST"<<endl;
+    int fd;
+    int data;
+    if((fd=serialOpen("/dev/ttyS0",115200))<0){
+        fprintf(stderr,"Unable to open serial device: %s\n",strerror(errno));
+	return 1;
+    }
+    printf("\nRasberry Pi UART Test");
+    for(int i=0;i<=50;i++){
+        data=serialGetchar(fd);
+        printf("%d",data);
+    }
     return 0;
 } 
