f574b1291b6f54a26607cd3eff8544b592f4d42d
SnovvyOwl <owl0614@naver.com>
2020-03-15 10:16:18 +0900

    ROLL good

--- test_code/AHRS_ebimu.cpp
+++ test_code/AHRS_ebimu.cpp
@@ -9,9 +9,7 @@ void AHRSread(float &ROLL,float &PITCH,float &YAW,int &fd);
 int main(){
     //ofstream fout;
     //fout.open("AHRS_data.txt");
-    
-    int fd;
-
+    int fd;//Serial
     float roll;
     float pitch;
     float yaw;
@@ -20,43 +18,51 @@ int main(){
 	return 1;
     }
     cout<<"\nRasberry Pi AHRS Test"<<endl;
-    for(int j=0;j<30;j++){
-        AHRSread(roll,pitch,yaw,fd);
-        cout<<"Roll: "<<roll<<endl<<"Pitch: "<<pitch<<endl<<"YAW: "<<yaw<<endl;
+    for(int j=0;j<10;j++){
+        AHRSread(roll,pitch,yaw,fd);//FUNCTION SENSOR NEED
+        //cout<<"Roll: "<<roll<<"\t"<<"Pitch: "<<pitch<<"\t"<<"YAW: "<<yaw<<endl;
     }
     return 0;
 } 
 void AHRSread(float &ROLL,float &PITCH,float &YAW,int &fd){
-    int i=0;
     int rawdata;
     string data;
     stringstream sout;
     do{
         rawdata=serialGetchar(fd);
     }while(rawdata!=42);//init ASCII 42 == "*"
-    while(rawdata!=44){
-        rawdata=serialGetchar(fd);
-        data=(char)rawdata;
-    }//DATA ROLL
-    sout<<data;
-    sout>>ROLL;
-    i=0;data="";
-    rawdata=serialGetchar(fd);
-    data[i]=(char)rawdata;
-    i++;
-    while(rawdata!=44){
-        rawdata=serialGetchar(fd);
-        data[i]=(char)rawdata;
-    };//DATA PITCH
-    sout<<data;
-    sout>>PITCH;
-    i=0;data="";
+    
+    //DATA ROLL
     do{
         rawdata=serialGetchar(fd);
-        data[i]=(char)rawdata;
-    }while(rawdata!=10);//DATA YAW
+        data+=(char)rawdata;
+    }while(rawdata!=44);
+    //cout<<data<<endl;
+    sout<<data;
+    sout>>ROLL;
+    sout.clear();
+    data="";
+    //DATA PITCH
+    do{
+        rawdata=serialGetchar(fd);
+        data+=(char)rawdata;
+    }while(rawdata!=44);
+    //cout<<data<<endl;
     sout<<data;
     sout>>PITCH;
-    i=0;data="";
+    cout<<PITCH<<endl<<endl;
+    
+    data="";
+    
+    //DATA YAW
+    rawdata=serialGetchar(fd);
+    data+=(char)rawdata;
+    while(rawdata!=10){
+        rawdata=serialGetchar(fd);
+        data+=(char)rawdata;
+    }
+    sout<<data;
+    sout>>PITCH;
+    data="";
 }
 
