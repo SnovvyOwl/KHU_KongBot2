b67886be1a415f0d733a2bef3e7add9247bfa138
SnovvyOwl <owl0614@naver.com>
2020-03-15 09:32:54 +0900

    AHRS_ebimu.cpp isn't Work now ,BUT,AHRs_test.cpp is working

--- test_code/AHRS_ebimu.cpp
+++ test_code/AHRS_ebimu.cpp
@@ -1,22 +1,62 @@
-
-#include<stdio.h>
-#include<string.h>
-#include<errno.h>
+#include<iostream>
+#include<string>
+#include<sstream>
+//#inlcude<fstream>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 using namespace std;
-int main()
-{
+void AHRSread(float &ROLL,float &PITCH,float &YAW,int &fd);
+int main(){
+    //ofstream fout;
+    //fout.open("AHRS_data.txt");
+    
     int fd;
-    int data;
+
+    float roll;
+    float pitch;
+    float yaw;
     if((fd=serialOpen("/dev/ttyUSB0",115200))<0){
-        fprintf(stderr,"Unable to open serial device: %s\n",strerror(errno));
+        cerr<<"Unable to open serial device"<<endl;
 	return 1;
     }
-    printf("\nRasberry Pi UART Test");
-    for(int i=0;i<=50;i++){
-        data=serialGetchar(fd);
-        printf("%d",data);
+    cout<<"\nRasberry Pi AHRS Test"<<endl;
+    for(int j=0;j<30;j++){
+        AHRSread(roll,pitch,yaw,fd);
+        cout<<"Roll: "<<roll<<endl<<"Pitch: "<<pitch<<endl<<"YAW: "<<yaw<<endl;
     }
     return 0;
 } 
+void AHRSread(float &ROLL,float &PITCH,float &YAW,int &fd){
+    int i=0;
+    int rawdata;
+    string data;
+    stringstream sout;
+    do{
+        rawdata=serialGetchar(fd);
+    }while(rawdata!=42);//init ASCII 42 == "*"
+    while(rawdata!=44){
+        rawdata=serialGetchar(fd);
+        data=(char)rawdata;
+    }//DATA ROLL
+    sout<<data;
+    sout>>ROLL;
+    i=0;data="";
+    rawdata=serialGetchar(fd);
+    data[i]=(char)rawdata;
+    i++;
+    while(rawdata!=44){
+        rawdata=serialGetchar(fd);
+        data[i]=(char)rawdata;
+    };//DATA PITCH
+    sout<<data;
+    sout>>PITCH;
+    i=0;data="";
+    do{
+        rawdata=serialGetchar(fd);
+        data[i]=(char)rawdata;
+    }while(rawdata!=10);//DATA YAW
+    sout<<data;
+    sout>>PITCH;
+    i=0;data="";
+}
+
