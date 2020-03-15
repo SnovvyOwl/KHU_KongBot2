aecc4e6d74bda86816f6195a807026a965c261c5
Oaflias <owl0614@naver.com>
2020-03-15 21:15:54 +0900

    testcode UPLOAD

--- testCode/Servo_test.cpp
+++ testCode/Servo_test.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
 #include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <softPwm.h>
-using namespace std;
-int main(){
-
-    return 0;
+ 
+int main ()
+{
+  int pos = 10 ;
+  int dir = 1 ;
+  if (wiringPiSetup() == -1) exit(1) ;  //init wiringPi
+ 
+  pinMode(0, OUTPUT) ; 
+  digitalWrite(0, LOW) ;  // 0 pin output LOW voltage
+  softPwmCreate(0, 0, 200) ; //SoftPWM... 
+  pinMode(1,OUTPUT);
+  while(1) {
+    pos += dir ;
+    if (pos < 10 || pos > 20) dir *= -1 ;
+    softPwmWrite(0, pos) ;
+    pwmWrite(1,pos);
+    delay(190) ;
+  }
+  return 0 ;
 }
