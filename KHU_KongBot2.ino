4aea60ead87201ce97b75ba6e496d10b0a7ba18d
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-03-21 10:43:30 +0900

    Arduino CODE
    모터 구매시 제대로 돌아가는지 확인해볼 필요가 있음
    전체적인 로봇의 흐름을 구상해봄

---
+++ KHU_KongBot2.ino
@@ -0,0 +1,80 @@
+#include <Servo.h>
+//Define Pin NUMBER
+#define PEND_R 10
+#define PEND_L 11
+#define IDU 9
+#define CTRL_ROLL 6
+//DEFINE FUNTION
+void init_motor();
+void reSYNC_motor();
+//DEFINE CMD 
+int CMD_IDU;
+int CMD_PEND_R;
+int CMD_PEND_L;
+int CMD_CTRL_ROLL;
+int reSYNC=0;
+//DEFINE SERVO
+Servo pendR;
+Servo pendL;
+Servo idu;
+Servo ctrl_roll;
+/* Command : IDU PEND_R PEND_L CTRL_ROLL
+ 1. init state 
+    RPI command Only IDU...
+ 2. Running state
+    RPI Calculate ALL parameter..
+    send servo Parameter to Arduino
+ 3. reSync
+*/
+String CMD;
+
+void setup() {
+  Serial.begin(115200);//Baudrate는 추후 결정!
+  while(Serial.available()<=0){
+    delay(10);
+  }
+  Serial.write("Arduino is Ready");
+  init_motor();
+}
+
+void loop() {
+  if(Serial.available()){
+    CMD_IDU=Serial.parseInt();
+    CMD_PEND_R=Serial.parseInt();
+    CMD_PEND_L=Serial.parseInt();
+    CMD_CTRL_ROLL=Serial.parseInt();
+    reSYNC=Serial.parseInt();
+    idu.write(CMD_IDU);
+    pendR.write(CMD_PEND_R);//0~180
+    pendL.write(CMD_PEND_L);//0~180
+    ctrl_roll.write(CMD_CTRL_ROLL);//0~180
+    if(reSYNC){
+      reSYNC_motor();
+      
+    }
+  }
+}
+
+void init_motor(){
+  pendR.attach(PEND_R);
+  pendL.attach(PEND_L);
+  idu.attach(IDU);
+  ctrl_roll.attach(CTRL_ROLL);
+  pendR.writeMicroseconds(1500);//700~2300
+  pendL.writeMicroseconds(1500);//700~2300
+  ctrl_roll.writeMicroseconds(1500);//700~2300
+  do{
+    CMD_IDU=Serial.parseInt();
+    idu.writeMicroseconds(CMD_IDU);
+    }while(CMD_IDU!=1500);  
+  Serial.write("KHU KONG BOT is READY");
+ }
+
+ void reSYNC_motor(){
+  CMD_IDU=idu.read();
+  CMD_PEND_R=pendR.read();
+  CMD_PEND_L=pendL.read();
+  CMD_CTRL_ROLL=ctrl_roll.read();
+  CMD=String(CMD_IDU)+String(',')+String(CMD_PEND_R)+String(',')+String(CMD_PEND_L)+String(',') +String(CMD_CTRL_ROLL);  
+  Serial.write(CMD.c_str());
+  }
