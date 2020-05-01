5d34c3f7f4e57dfc8edb561f4ae92ccb0af992e1
SnovvyOwl <owl0614@naver.com>
2020-05-01 09:56:24 +0900

    To measure anglur velocity

--- sampleCode/Encoder_angularVel.cpp
+++ sampleCode/Encoder_angularVel.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<wiringPi.h>
-
+#include<ctime>
+#include<signal.h>
 #define PhaseA 21
 #define PhaseB 22
 using namespace std;
@@ -8,21 +9,20 @@ using namespace std;
 double encoder_pulse = 0;
 double angle = 0;
 int encoder_pos = 0;
-
+float vel=0;
 bool State_A = 0;
 bool State_B = 0;
 void Interrupt_A();
-void Interrupt_B();
+void Interrupt_B(); 
 
 int main(){
 	wiringPiSetup();
 	pinMode(PhaseA, INPUT);
 	pinMode(PhaseB, INPUT);
-	wiringPiISR(PhaseA, INT_EDGE_BOTH, &Interrupt_A);//  encoder pin on interrupt 0 (pin 2)
-	wiringPiISR(PhaseB, INT_EDGE_BOTH, &Interrupt_B); //  encoder pin on interrupt 1 (pin 3)
-	while(1) {
+	wiringPiISR(PhaseA, INT_EDGE_BOTH, &Interrupt_A);
+	wiringPiISR(PhaseB, INT_EDGE_BOTH, &Interrupt_B);
 		encoder_pulse = (float)360 / (3600 * 4); //3600 PPR
-		cout << "Encoder Pos : " << encoder_pos << "\tAngle : " << angle << "\tPulse : " << encoder_pulse << "\n";
+		cout << "Encoder Pos : " << encoder_pos << "\tAngle : " << angle << "\t Vel : " << vel << "\n";
 	}
 }
 
