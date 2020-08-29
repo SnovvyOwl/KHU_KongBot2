a07d7054b168302964be258cc9cfd0dd66cca4ab
Oaflias <owl0614@naver.com>
2020-08-29 19:59:45 +0900

    rkwk

--- motor_test/SRIVC.py
+++ motor_test/SRIVC.py
@@ -3,5 +3,15 @@ import matplotlib as plt
 import numpy as np
 import os
 os.chdir(os.getcwd())
-y= np.loadtxt("KHU_KongBot2/motor_test/RefinedY.txt",delimiter=",")
-u= np.loadtxt("KHU_KongBot2/motor_test/RefinedU.txt",delimiter=",")
+y= np.loadtxt("motor_test/RefinedY.txt",delimiter=",")
+u= np.loadtxt("motor_test/RefinedU.txt",delimiter=",")
+#DP CONTROLLER
+A=np.array([1,1,1,1])
+n=1
+B=np.reshape(1,n)
+s=control.TransferFunction.s
+sys=control.tf(B,A)
+sys=control.tf2io(sys)
+#sys=B[0]/(A[0]*s**3+A[1]*s**2+A[2]*s+A[3])
+u.sort(axis=0)
+yf=control.input_output_response(sys,u[:,0])
