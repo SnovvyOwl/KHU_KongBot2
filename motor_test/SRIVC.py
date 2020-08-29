645f3ca76f52092a658270fa372ccff043b48f51
Oaflias <owl0614@naver.com>
2020-08-29 20:48:06 +0900

    DATA READ SUCESS

--- motor_test/SRIVC.py
+++ motor_test/SRIVC.py
@@ -1,3 +1,15 @@
+""" THIS CODE IS Written by SeongWon Lee 
+% THIS CODE IS based Practical Modeling and System Identification of R/C
+% Servo Motors( 18th IEEE International Conference on Control Applications
+% Part of 2009 IEEE Multi-conference on Systems and Control
+% Saint Petersburg, Russia, July 8-10, 2009)
+
+Dependency: python Control Sysytem Library
+            numpy
+            scipy
+            matplotlib
+
+"""
 import control
 import matplotlib as plt
 import numpy as np
@@ -14,4 +26,5 @@ sys=control.tf(B,A)
 sys=control.tf2io(sys)
 #sys=B[0]/(A[0]*s**3+A[1]*s**2+A[2]*s+A[3])
 u.sort(axis=0)
-yf=control.input_output_response(sys,u[:,0])
+yf=control.input_output_response(sys,u[:,0],u[:,1])
+print(yf)
