88149ebc8e62d8efc8560dfa620100ebbb5022c6
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-08-29 15:32:30 +0900

    data REFINED

--- motor_test/SRIVCservomoter.m
+++ motor_test/SRIVCservomoter.m
@@ -6,25 +6,28 @@
 clc;clear;clf;
 angle=-xlsread("angle.csv");
 time=xlsread("time.csv");
-y=[(time(:,1)-2624), angle];
+y=[(time(:,1)-2624)./1000, angle];
 y=unique(y,'rows','stable'); % REFINED DATA;
 
 %step function
 u=[y(:,1),zeros(21235,1)];
 %u=unique(u,"rows","stable");
-t1=find(u(:,1)==1009);
-t2=find(u(:,1)==3019);
+t1=find(u(:,1)==1.009);
+t2=find(u(:,1)==3.019);
 u(t1:t2,2)=8.3;
-t1=find(u(:,1)==5006);
-t2=find(u(:,1)==7009);
+t1=find(u(:,1)==5.006);
+t2=find(u(:,1)==7.009);
 u(t1:t2,2)=8.3;
-t1=find(u(:,1)==9008);
-t2=find(u(:,1)==11005);
+t1=find(u(:,1)==9.008);
+t2=find(u(:,1)==11.005);
 u(t1:t2,2)=16.775;
-t1=find(u(:,1)==12991);
-t2=find(u(:,1)==15009);
+t1=find(u(:,1)==12.991);
+t2=find(u(:,1)==15.009);
 u(t1:t2,2)=16.775;
-t1=find(u(:,1)==16998);
-t2=find(u(:,1)==19000);
+t1=find(u(:,1)==16.998);
+t2=find(u(:,1)==19.000);
 u(t1:t2,2)=16.775;
 plot(y(:,1),y(:,2),u(:,1),u(:,2))
+clear angle t1 t2 time
+dlmwrite("RefinedY.txt",y)
+dlmwrite("RefinedU.txt",u)
