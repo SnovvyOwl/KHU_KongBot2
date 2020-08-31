f3231e489adc674f7180e9abb03508d994dcad9b
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-08-31 21:05:51 +0900

    Update SRIVC_MULTI.py

--- motor_test/SRIVC_MULTI.py
+++ motor_test/SRIVC_MULTI.py
@@ -14,8 +14,9 @@ Dependency: python Control Sysytem Library
 import control
 from matplotlib import pyplot as plt
 import numpy as np
-from multiprocessing import Process, Queue ,freeze_support
+from multiprocessing import Process, Queue, freeze_support
 import os
+
 
 def fil_y(pi_a, t, y, yf):
     yf.put(control.input_output_response(pi_a, t, y))
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     t = y[0:3000, 0]
     y = y[0:3000, 1]
     u = u[0:3000, 1]
-    yf = np.zeros((2, len(t)))
+    yf_p = np.zeros((2, len(t)))
     # DP CONTROLLER
     n = 3
     m = 0
@@ -52,47 +53,48 @@ if __name__ == "__main__":
     phi = np.zeros((n + m + 1, len(u)))
     phi_hat = np.zeros((n + m + 1, len(u)))
     j = 1
-    while (j <= 2):
-        A = theta[0:n]
-        A = np.insert(A, 0, 1)
-        B = theta[n]
+    while (j <= 10):
         for i in range(n):
             p_i = np.zeros(n + 1)
-            p_i[i + 1] = 1
-            Bp = control.tf(B, 1)
-            filter_y = control.tf(p_i, A)
-            filter_y = control.tf2io(filter_y)
-            pro1 = Process(target=fil_y, args=(filter_y, t, y, result))
+            p_i[i + 1] = -1
+            sys_hat = control.tf(B, A)
+            Pn_Ap = control.tf(p_i, A)  # P^n/A(p)
+            Pn_Ap_IO = control.tf2io(Pn_Ap)
+            pro1 = Process(target=fil_y, args=(Pn_Ap_IO, t, y, result))  # calc P^n*Y_f
             pro1.start()
-            estimate_yf = Bp * filter_y
-            estimate_yf = control.tf2io(estimate_yf)
-            yf_hat = control.input_output_response(estimate_yf, t, y)
-            yf = result.get()
+            PnxXf = Pn_Ap * sys_hat  # P^n/A(p)*B(p)/A(p)
+            PnxXf = control.tf2io(PnxXf)
+            PnxXf = control.input_output_response(PnxXf, t, u)  # calc P^n*X_f
+            PnxYf = result.get()
             pro1.join()
-            phi[i] = -yf[1]
-            phi_hat[i] = -yf_hat[1]
+            phi[i] = PnxYf[1]
+            phi_hat[i] = PnxXf[1]
             pro1.close()
-
 
         for i in range(m + 1):
             p_i = np.zeros(m + 1)
             p_i[i] = 1
-            filter_u = control.tf(p_i, A)
-            filter_u = control.tf2io(filter_u)
-            uf = control.input_output_response(filter_u, t, u)
-            phi[n + i] = uf[1]
-            phi_hat[n + i] = uf[1]
+            Pm_Ap = control.tf(p_i, A) # P^m/A(p)
+            PmxUf = control.tf2io(Pm_Ap)
+            PmxUf = control.input_output_response(PmxUf, t, u)
+            phi[n + i] = PmxUf[1]
+            phi_hat[n + i] = PmxUf[1]
 
+        Pn = np.zeros(3)
+        Pn = np.insert(Pn, 0, 1)
+        yf = control.tf(Pn, A)
+        yf = control.tf2io(yf)
+        yf = control.input_output_response(yf, t, y)
         front = np.matmul(phi_hat, phi.T)
-        back = np.matmul(phi, yf[1].T)
+        back = np.matmul(phi_hat, yf[1].T)
         new_theta = np.matmul(np.linalg.inv(front), back)
         theta = new_theta
         j = j + 1
+        A = theta[0:n]
+        A = np.insert(A, 0, 1)
+        B = theta[n]
         print(j)
 
-    A = theta[0:n]
-    A = np.insert(A, 0, 1)
-    B = theta[n]
     sys = control.tf(B, A)
     sys = control.tf2io(sys)
     est_y = control.input_output_response(sys, t, u)
