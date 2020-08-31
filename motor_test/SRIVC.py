2db5c6d0dd78a8438278992102fbe66727a236b9
Oaflias <owl0614@naver.com>
2020-08-31 13:26:01 +0900

    complete

--- motor_test/SRIVC.py
+++ motor_test/SRIVC.py
@@ -17,34 +17,34 @@ import os
 os.chdir(os.getcwd())
 y= np.loadtxt("KHU_KongBot2/motor_test/RefinedY.txt",delimiter=",")
 u= np.loadtxt("KHU_KongBot2/motor_test/RefinedU.txt",delimiter=",")
-y=y[0:3000]
-u=u[0:3000]
-print(u[:,1])
+yr=y
+ur=u
+t=y[0:3000,0]
+y=y[0:3000,1]
+u=u[0:3000,1]
+
 #DP CONTROLLER
 n=3
-theta=np.array([45.29,1413,17970,18130])
-M=1000
-j=1
-flag=1
 m=0
+theta=np.array([45.29,1413,17970,18130]) #INITIAL ESTIMATE
+
 phi=np.zeros((n+m+1,len(u)))
 phi_hat=np.zeros((n+m+1,len(u)))
-#plt.plot(u[:,0],u[:,1])
-#plt.show()print(y)
-while (j<=2):
+j=1
+while (j<=100):
     A=theta[0:n]
     A=np.insert(A,0,1)
     B=theta[n]
     for i in range(n):
         p_i=np.zeros(n+1)
-        p_i[i]=1
+        p_i[i+1]=1
         Bp=control.tf(B,1)
         filter_y=control.tf(p_i,A)
         estimate_yf=Bp*filter_y
         filter_y=control.tf2io(filter_y)
         estimate_yf=control.tf2io(estimate_yf)
-        yf=control.input_output_response(filter_y,y[:,0],y[:,1])
-        yf_hat=control.input_output_response(estimate_yf,y[:,0],y[:,1])
+        yf=control.input_output_response(filter_y,t,y)
+        yf_hat=control.input_output_response(estimate_yf,t,y)
         phi[i]=-yf[1]
         phi_hat[i]=-yf_hat[1]
 
@@ -53,17 +53,14 @@ while (j<=2):
         p_i[i]=1
         filter_u=control.tf(p_i,A)
         filter_u=control.tf2io(filter_u)
-        uf=control.input_output_response(filter_u,u[:,0],u[:,1])
-
+        uf=control.input_output_response(filter_u,t,u)
         phi[n+i]=uf[1]
         phi_hat[n+i]=uf[1]
-        #print(phi)
+    
     front=np.matmul(phi_hat,phi.T)
     back=np.matmul(phi,yf[1].T)
     new_theta=np.matmul(np.linalg.inv(front),back)
     theta=new_theta
-    print(theta)
-
     j=j+1
 
 A=theta[0:n]
@@ -71,6 +68,6 @@ A=np.insert(A,0,1)
 B=theta[n]
 sys=control.tf(B,A)
 sys=control.tf2io(sys)
-sys=control.input_output_response(sys,u[:,0],u[:,1])
-plt.plot(sys[0],sys[1])
+est_y=control.input_output_response(sys,t,u)
+plt.plot(t,u,est_y[0],est_y[1])
 plt.show()
