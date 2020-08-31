cecc1c22deebb0b40a8d2dd0e65e457c6a251795
Oaflias <owl0614@naver.com>
2020-08-31 12:03:49 +0900

    update

--- motor_test/SRIVC.py
+++ motor_test/SRIVC.py
@@ -11,49 +11,66 @@ Dependency: python Control Sysytem Library
 
 """
 import control
-import matplotlib as plt
+from matplotlib import pyplot as plt
 import numpy as np
 import os
 os.chdir(os.getcwd())
 y= np.loadtxt("KHU_KongBot2/motor_test/RefinedY.txt",delimiter=",")
 u= np.loadtxt("KHU_KongBot2/motor_test/RefinedU.txt",delimiter=",")
+y=y[0:3000]
+u=u[0:3000]
+print(u[:,1])
 #DP CONTROLLER
-n=4
-theta=np.array([1,1,1,1,1])
+n=3
+theta=np.array([45.29,1413,17970,18130])
 M=1000
 j=1
 flag=1
-A=theta[1:]
-B=theta[0]
-m=1
-phi=np.zeros((n+m-1,15387))
-phi_hat=np.zeros((n+m-1,15387))
-while ((flag==1) and (j<=2)):
-    for i in range(n-1):
-        p_i=np.zeros(n)
+m=0
+phi=np.zeros((n+m+1,len(u)))
+phi_hat=np.zeros((n+m+1,len(u)))
+#plt.plot(u[:,0],u[:,1])
+#plt.show()print(y)
+while (j<=2):
+    A=theta[0:n]
+    A=np.insert(A,0,1)
+    B=theta[n]
+    for i in range(n):
+        p_i=np.zeros(n+1)
         p_i[i]=1
         Bp=control.tf(B,1)
         filter_y=control.tf(p_i,A)
-        estimate_y=Bp*filter_y
+        estimate_yf=Bp*filter_y
         filter_y=control.tf2io(filter_y)
-        estimate_y=control.tf2io(estimate_y)
+        estimate_yf=control.tf2io(estimate_yf)
         yf=control.input_output_response(filter_y,y[:,0],y[:,1])
-        yf_hat=control.input_output_response(estimate_y,y[:,0],y[:,1])
+        yf_hat=control.input_output_response(estimate_yf,y[:,0],y[:,1])
         phi[i]=-yf[1]
         phi_hat[i]=-yf_hat[1]
 
-    for i in range(m):
-        p_i=np.zeros(m)
+    for i in range(m+1):
+        p_i=np.zeros(m+1)
         p_i[i]=1
         filter_u=control.tf(p_i,A)
         filter_u=control.tf2io(filter_u)
         uf=control.input_output_response(filter_u,u[:,0],u[:,1])
-        phi[n+i-1]=uf[1]
-        phi_hat[n+i-1]=uf[1]
 
+        phi[n+i]=uf[1]
+        phi_hat[n+i]=uf[1]
+        #print(phi)
     front=np.matmul(phi_hat,phi.T)
-    back=np.matmul(phi_hat,yf[1].T)
+    back=np.matmul(phi,yf[1].T)
     new_theta=np.matmul(np.linalg.inv(front),back)
-    print(new_theta)
+    theta=new_theta
+    print(theta)
+
     j=j+1
 
+A=theta[0:n]
+A=np.insert(A,0,1)
+B=theta[n]
+sys=control.tf(B,A)
+sys=control.tf2io(sys)
+sys=control.input_output_response(sys,u[:,0],u[:,1])
+plt.plot(sys[0],sys[1])
+plt.show()
