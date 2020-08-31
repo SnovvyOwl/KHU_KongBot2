3ee064ec0744a4b6e507b23cfaddb82c4bbfe05f
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-08-31 18:38:45 +0900

    Update SRIVC_MULTI.py

--- motor_test/SRIVC_MULTI.py
+++ motor_test/SRIVC_MULTI.py
@@ -14,82 +14,88 @@ Dependency: python Control Sysytem Library
 import control
 from matplotlib import pyplot as plt
 import numpy as np
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue ,freeze_support
 import os
-os.chdir(os.getcwd())
-y= np.loadtxt("KHU_KongBot2/motor_test/RefinedY.txt",delimiter=",")
-u= np.loadtxt("KHU_KongBot2/motor_test/RefinedU.txt",delimiter=",")
-yr=y
-ur=u
-t=y[0:3000,0]
-y=y[0:3000,1]
-u=u[0:3000,1]
-yf=np.zeros((2,len(t)))
-def fil_y(pi_A,t,y,yf):
-    yf.put(control.input_output_response(pi_A,t,y))
+
+def fil_y(pi_a, t, y, yf):
+    yf.put(control.input_output_response(pi_a, t, y))
     return
 
-#DP CONTROLLER
-n=3
-m=0
-theta=np.array([45.29,1413,17970,18130]) #INITIAL ESTIMATE
-A=theta[0:n]
-A=np.insert(A,0,1)
-B=theta[n]
-init=control.tf(B,A)
-init=control.tf2io(init)
-first=control.input_output_response(init,t,u)
-plt.plot(first[0],first[1])
-plt.show()
 
-result=Queue()
-phi=np.zeros((n+m+1,len(u)))
-phi_hat=np.zeros((n+m+1,len(u)))
-j=1
-while (j<=3):
-    A=theta[0:n]
-    A=np.insert(A,0,1)
-    B=theta[n]
-    for i in range(n):
-        p_i=np.zeros(n+1)
-        p_i[i+1]=1
-        Bp=control.tf(B,1)
-        filter_y=control.tf(p_i,A)
-        filter_y=control.tf2io(filter_y)
-        pro1 = Process(target=fil_y, args=(filter_y,t,y,result))
-        pro1.start()
-        estimate_yf=Bp*filter_y
-        estimate_yf=control.tf2io(estimate_yf)
-        yf_hat=control.input_output_response(estimate_yf,t,y)
-        yf=result.get()
-        pro1.join()
-        phi[i]=-yf[1]
-        phi_hat[i]=-yf_hat[1]
-        pro1.close()
+if __name__ == "__main__":
+    freeze_support()
+    y = np.loadtxt("RefinedY.txt", delimiter=",")
+    u = np.loadtxt("RefinedU.txt", delimiter=",")
+    yr = y
+    ur = u
+    t = y[0:3000, 0]
+    y = y[0:3000, 1]
+    u = u[0:3000, 1]
+    yf = np.zeros((2, len(t)))
+    # DP CONTROLLER
+    n = 3
+    m = 0
+    theta = np.array([45.29, 1413, 17970, 18130])  # INITIAL ESTIMATE
+    A = theta[0:n]
+    A = np.insert(A, 0, 1)
+    B = theta[n]
 
-    for i in range(m+1):
-        p_i=np.zeros(m+1)
-        p_i[i]=1
-        filter_u=control.tf(p_i,A)
-        filter_u=control.tf2io(filter_u)
-        uf=control.input_output_response(filter_u,t,u)
-        phi[n+i]=uf[1]
-        phi_hat[n+i]=uf[1]
-   
-    front=np.matmul(phi_hat,phi.T)
-    back=np.matmul(phi,yf[1].T)
-    new_theta=np.matmul(np.linalg.inv(front),back)
-    theta=new_theta
-    j=j+1
-    print(j)
+    """
+    init = control.tf(B, A)
+    init = control.tf2io(init)
+    first = control.input_output_response(init, t, u)
+    plt.plot(first[0], first[1])
+    plt.show()
+    """
+
+    result = Queue()
+    phi = np.zeros((n + m + 1, len(u)))
+    phi_hat = np.zeros((n + m + 1, len(u)))
+    j = 1
+    while (j <= 2):
+        A = theta[0:n]
+        A = np.insert(A, 0, 1)
+        B = theta[n]
+        for i in range(n):
+            p_i = np.zeros(n + 1)
+            p_i[i + 1] = 1
+            Bp = control.tf(B, 1)
+            filter_y = control.tf(p_i, A)
+            filter_y = control.tf2io(filter_y)
+            pro1 = Process(target=fil_y, args=(filter_y, t, y, result))
+            pro1.start()
+            estimate_yf = Bp * filter_y
+            estimate_yf = control.tf2io(estimate_yf)
+            yf_hat = control.input_output_response(estimate_yf, t, y)
+            yf = result.get()
+            pro1.join()
+            phi[i] = -yf[1]
+            phi_hat[i] = -yf_hat[1]
+            pro1.close()
 
 
-A=theta[0:n]
-A=np.insert(A,0,1)
-B=theta[n]
-sys=control.tf(B,A)
-sys=control.tf2io(sys)
-est_y=control.input_output_response(sys,t,u)
-plt.plot(est_y[0],est_y[1])
-plt.show()
-print("end")
+        for i in range(m + 1):
+            p_i = np.zeros(m + 1)
+            p_i[i] = 1
+            filter_u = control.tf(p_i, A)
+            filter_u = control.tf2io(filter_u)
+            uf = control.input_output_response(filter_u, t, u)
+            phi[n + i] = uf[1]
+            phi_hat[n + i] = uf[1]
+
+        front = np.matmul(phi_hat, phi.T)
+        back = np.matmul(phi, yf[1].T)
+        new_theta = np.matmul(np.linalg.inv(front), back)
+        theta = new_theta
+        j = j + 1
+        print(j)
+
+    A = theta[0:n]
+    A = np.insert(A, 0, 1)
+    B = theta[n]
+    sys = control.tf(B, A)
+    sys = control.tf2io(sys)
+    est_y = control.input_output_response(sys, t, u)
+    plt.plot(est_y[0], est_y[1])
+    plt.show()
+    print("end")
