ea085800ca13bf15044c94b1b00bb475246cddac
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2021-01-26 09:16:43 +0900

    Update largrangian_2d_straight.py

--- Dynamics_Modeling/largrangian_2d_straight.py
+++ Dynamics_Modeling/largrangian_2d_straight.py
@@ -25,7 +25,7 @@ th_s,th_im,th_pm=dynamicsymbols("\\theta_{s} \\theta_{im} \\theta_{pm}")
 # Energy Equation
 Ts=1/2*ms*(Rs*  th_s.diff())**2+1/2*Js* th_s.diff()**2
 Ti=1/2*mi*(Rs*  th_s.diff())**2+1/2*Ji*( th_s.diff()+  th_im.diff())**2
-Tp=1/2*mp*((Rs* th_s.diff()+( th_s.diff()+ th_im.diff()+ th_pm.diff())*cos(th_s+th_im+th_pm))**2+( th_s.diff()+ th_im.diff()+ th_pm.diff()*sin(th_s+th_im+th_pm))**2)+1/2*Jp*( th_s.diff()+ th_im.diff()+ th_pm.diff())**2
+Tp=1/2*mp*((Rs* th_s.diff()+lp*( th_s.diff()+ th_im.diff()+ th_pm.diff())*cos(th_s+th_im+th_pm))**2+lp*( th_s.diff()+ th_im.diff()+ th_pm.diff()*sin(th_s+th_im+th_pm))**2)+1/2*Jp*( th_s.diff()+ th_im.diff()+ th_pm.diff())**2
 T=Ts+Ti+Tp
 U=-mp*g*lp*cos(th_s+th_im+th_pm)
 L=T-U
