f45aab172281150f1495da4a5b796f737b5005bf
seongwonlee <owl0614@naver.com>
2021-05-20 20:44:57 +0900

    modi

--- testCode/KF_Test/KF_Test_SW.cpp
+++ testCode/KF_Test/KF_Test_SW.cpp
@@ -10,7 +10,7 @@
 #define T 0.1
 using namespace std;
 using namespace cv;
-void KF_filter(Mat &x_bar, Mat &x_hat, Mat &X, Mat &U, Mat &z_hat, Mat &Z,Mat &p_bar,Mat &p_hat,Mat S_bar,Matx44d Q, double R, Matx44d &Qf,Mat &KK);//KF
+void KF_filter(Mat &x_bar, Mat &x_hat, Mat &X, Mat &U, Mat &z_hat, Mat &Z,Mat &p_bar,Mat &p_hat,Mat &S_bar,Matx44d Q, double R, Matx44d &Qf,Mat &KK);//KF
 //GLOBAL VARIABLE
 //NORMAL DISTRIBUTION RANDOM NUMBER GENERATOR
 default_random_engine generator;
@@ -26,16 +26,18 @@ int main(){
     fout.open("data.txt");
     float D=0;
     //Initial Conditions
+    //Matx41d X(0,0,0,0);
     Mat X=(Mat_<double>(4,1)<<0,0,0,0);
     Mat X_hat=(Mat_<double>(4,1)<<0,0,0,0);
-    Mat P = Mat::eye(X_hat.rows, X_hat.rows, CV_32F)*1000;
-    Mat X_bar=(Mat_<double>(4,1)<<0,0,1*DEG2RAD,0);
+    Mat P = Mat::eye(X_hat.rows, X_hat.rows, CV_64FC1)*1000;
     sqrt(P,P);
     double W=dist_W(generator);
-    //X_bar=X+P*W; 문제의 줄 diag???
-    P = Mat::eye(X_hat.rows, X_hat.rows, CV_32F)*1000;
-    Mat p_bar = Mat::eye(X_hat.rows, X_hat.rows, CV_32F)*1000;
-    Mat p_hat = Mat::eye(X_hat.rows, X_hat.rows, CV_32F)*1000;
+    Mat X_bar;
+    X_bar.copySize(P.mul(W).diag());
+    X_bar=X+P.mul(W).diag();
+    P = Mat::eye(X_hat.rows, X_hat.rows, CV_64FC1)*1000;
+    Mat p_bar = Mat::eye(X_hat.rows, X_hat.rows, CV_64FC1)*1000;
+    Mat p_hat = Mat::eye(X_hat.rows, X_hat.rows, CV_64FC1)*1000;
     //Noise
     Matx44d Q(pow(T,4)/24,pow(T,3)/6,pow(T,2)/2,T,pow(T,4)/24,pow(T,3)/6,pow(T,2)/2,T,pow(T,4)/24,pow(T,3)/6,pow(T,2)/2,T,pow(T,4)/24,pow(T,3)/6,pow(T,2)/2,T); //SYSYTEM NOISE 바꿀 필요가 있어보임
     double R= SIGMA_V*SIGMA_V;
@@ -45,21 +47,21 @@ int main(){
     Mat Z;
     Mat KK;
     Mat S_bar;
-    //KF_filter(X_bar,X_hat,X,u,z_hat,Z,p_bar,p_hat,S_bar,Q,R,Qf,KK);
-    /*
+    
+    
     for(float i=0;i<100;i=i+T){
-        fout<<X_true<<endl;
-        KF_filter(F,G,H,X_true,X_hat,u,G_w,W,V);
+        fout<<X<<endl;
+        KF_filter(X_bar,X_hat,X,u,z_hat,Z,p_bar,p_hat,S_bar,Q,R,Qf,KK);
     }
-    */
+    
     return 0;
 }
-void KF_filter(Mat &x_bar, Mat &x_hat, Mat &X, Mat &U, Mat &z_hat, Mat &Z,Mat &p_bar,Mat &p_hat,Mat S_bar,Matx44d Q, double R, Matx44d &Qf,Mat &KK){
+void KF_filter(Mat &x_bar, Mat &x_hat, Mat &X, Mat &U, Mat &z_hat, Mat &Z,Mat &p_bar,Mat &p_hat,Mat &S_bar,Matx44d Q, double R, Matx44d &Qf,Mat &KK){
     double V= dist_V(generator);
     Z=H*X+sqrt(R)*V; //MESUREMENT MODEL
     //==================
     //MESUREMENT UPDATE
-    //==================
+    //=================
     z_hat=H*x_bar;
     Mat S=H*p_bar*H.t()+R;
     p_hat=p_bar-p_bar*H.t()*S.inv()*H*p_bar;
