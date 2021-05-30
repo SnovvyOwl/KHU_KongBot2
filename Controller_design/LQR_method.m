08d8ef4c947e5b1fa65fe1db4d356c9b92b5cb58
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2021-05-30 22:22:42 +0900

    ddd

--- Controller_design/LQR_method.m
+++ Controller_design/LQR_method.m
@@ -7,34 +7,36 @@ A= [0 1 0 0
 B=[0; -4.888;0 ;1470.59];
 C=[0 1 0 0];
 D=0;
-Q = [10 0 0 0
-     0 1 0 0
-     0 0 1 0
-     0 0 0 1];
+Q = [15000 0 0 0
+     0 100000 0 0
+     0 0 1000 0
+     0 0 0 100]
  R = [1];
 
  [K,S,e]= lqr(A,B,Q,R)
-k1=K(1); k2=K(2);k3=K(3);k4=K(4)
+% k1=K(1); k2=K(2);k3=K(3);k4=K(4)
 
-AA = A - B*K;
-BB = B*k1;
-CC=C;
-DD=D;
-t = 0:0.01:10;
-[y,x,t]=step(AA,BB,CC,DD,1,t);
+% AA = A - B*K;
+% BB = B*k1;
+% CC=C;
+% DD=D;
+% t = 0:0.01:10;
+% [y,x,t]=step(AA,BB,CC,DD,1,t);
+% plot(t,x)
+% legend('x1','x2','x3','x4')
+% grid
 
 d2r = pi/180; r2d = 1/d2r;
 
+sys=ss(A-B*K,eye(4),eye(4),eye(4))
+t=0:0.01:500;
+x=initial(sys,[0;1;0;0],t);
+X1=[1 0 0 0]*x';
+X2=[0 1 0 0]*x';
+X3=[0 0 1 0]*x';
+X4=[0 0 0 1]*x';
 
-% sys=ss(A-B*K,eye(4),eye(4),eye(4))
-% t=0:0.01:15;
-% x=initial(sys,[0;d2r;0;0],t)
-%X1=[1 0 0 0]*x';
-% X2=[0 1 0 0]*x';
-%X3=[0 0 1 0]*x';
-%X4=[0 0 0 1]*x';
-% 
- %subplot(2,2,1);plot(t,x1);grid
- %subplot(2,2,2);plot(t,x2);grid
-%subplot(2,2,3);plot(t,x3);grid
- %subplot(2,2,4);plot(t,x4);grid
+ subplot(2,2,1);plot(t,X1);grid
+ subplot(2,2,2);plot(t,X2);grid
+ subplot(2,2,3);plot(t,X3);grid
+ subplot(2,2,4);plot(t,X4);grid
