6bb39d08dd9ba74dea9021178e89b710d35fe559
SnovvyOwl <owl0614@naver.com>
2021-07-11 21:42:42 +0900

    commit

--- state.h
+++ state.h
@@ -7,29 +7,28 @@ class Pendulum{
         float pendulumTheta[4]={0,0,0,0};
     public:
         Pendulum(float state){
-            pendulumTheta[0]=state;
-            pendulumTheta[1]=state;
-            pendulumTheta[2]=state;
-            pendulumTheta[3]=state;
+            pendulumTheta[0]=state;//Current
+            pendulumTheta[1]=state;//K-1
+            pendulumTheta[2]=state;//K-2
+            pendulumTheta[3]=state;//K-3
         }
         void clear(){
-            input[0]=0;
-            input[1]=0;
-            input[2]=0;
-            input[3]=0;
+            input[0]=0;//Current
+            input[1]=0;//K-1
+            input[2]=0;//K-2
+            input[3]=0;//K-3
             pendulumTheta[0]=0;
             pendulumTheta[1]=0;
             pendulumTheta[2]=0;
             pendulumTheta[3]=0;
         }
-
-        
         int shell2pen(float curInput){
             //Pendulum Transfer Funtion  
-            /*             452.2 s + 5781                                   theta_s
+            /*             452.2 s + 5781                                   theta_p
                 --------------------------------                =    --------------------------------
-                    s^3 + 38.85 s^2 + 851.1 s + 5774                        theta_p
-
+                    s^3 + 38.85 s^2 + 851.1 s + 5774                        theta_s
+            
+            
                 0.3199 z^3 + 0.5695 z^2 + 0.1791 z - 0.07042 
                 --------------------------------------------      
                  z^3 - 0.1121 z^2 + 0.1891 z - 0.08002                 
