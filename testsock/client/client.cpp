8b85c5a83e42b4ed53696aad08e78397b70eb1b9
seongwonlee <owl0614@naver.com>
2021-08-26 00:42:28 +0900

    set

--- testsock/client/client.cpp
+++ testsock/client/client.cpp
@@ -9,36 +9,36 @@ Client::Client(const char *hostname, const int _port)
     server_addr.sin_port = htons(_port);
     server_addr.sin_addr.s_addr=*(long*)(he->h_addr_list[0]);
     if(client==-1){
-        cerr<< "\n Socket creation error \n";
+        cerr<< "\n Socket creation error \n";//소켓이 만들어지지 않았을 경우 프로그램 종료
         msgReceive="quit";
 	    exit(1);
     }
     if (connect(client, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){    
-        cerr<<"\nConnection Failed \n"; 
+        cerr<<"\nConnection Failed \n"; //서버와 연결되지 않았을 경우 프로그램 종료
         msgReceive="quit";
         exit(1);
     }
-    runClient();
+    runClient();//runClient함수 호출
 }
        
 void Client::runClient(){
-    thread sockReceive([&](){receive_send();});
+    thread sockReceive([&](){receive_send();});//소켓통신을 통해 데이터만 주고 받는 쓰레드
     int i=0;
-    sockReceive.detach();
+    sockReceive.detach();//메인 쓰레드와 생성된 쓰레드가 완전히 독립적으로 작동함.
     do{ 
         msgSend=to_string(i)+"\n";
-        send(client,msgSend.c_str(),msgSend.size(),0); 
-        i++;
-    } while (msgReceive[0] != 'q');
-    close(client);
+        send(client,msgSend.c_str(),msgSend.size(),0); //서버로 i를 보냄
+        cout<<msgReceive<<endl;//서버에서 보낸 메세지 출력
+    } while (msgReceive!= "q");//서버에서 받은 데이터가 q로 시작될경우 종료
+    close(client); //보통 서버가 먼저 꺼지면 프로그램이 자동 종료 된다.
 }
+
 void Client::receive_send(){
     //Socket receive[THREAD 2]
-    char buffer[BUFF_SIZE]={0};
+    char buffer[BUFF_SIZE]={0};//서버에서 보낸 데이터를 받을 버퍼
     do {
-        read(client,buffer,BUFF_SIZE);
-        msgReceive=buffer;
-        //serialPuts(Nano,CMD.c_str());
-        buffer[0]={0,};
+        read(client,buffer,BUFF_SIZE);//받은 데이터를 버퍼에 저장
+        msgReceive=buffer;//버퍼를 string으로 변환
+        buffer[0]={0,};//버퍼 초기화
     } while ( msgReceive != "q");
 }
--- testsock/server/server.h
+++ testsock/server/server.h
@@ -13,23 +13,23 @@ using namespace std;
 class Server
 {
 private:
-    char *ip;
-    int port = 13000;
-    int server = 0;
-    int client = 0;
-    struct sockaddr_in client_addr;
-    struct sockaddr_in server_addr;
-    char CMD;
-    socklen_t client_addr_len = sizeof(client_addr);
-    string msgReceive = "";
-    string msgSend = "";
+    char *ip;//서버 IP주소
+    int port = 13000;//포트번호
+    int server = 0;//서버 소켓
+    int client = 0;//클라이언트 소켓
+    struct sockaddr_in client_addr;//클라이언트 소켓 주소  구조체
+    struct sockaddr_in server_addr;//서버 소켓 주소 구조체
+    char CMD;//키보드 입력 명령어
+    socklen_t client_addr_len = sizeof(client_addr);//소켓주소 길이
+    string msgReceive = "";//클라이언트에서 보낸 데이터
+    string msgSend = "";//클라이언트로 보낼 데이터
 
 public:
-    Server(const char *_ip, int _port);
-    void startServer();
-    void runServer();
-    void receiving();
-    void sending();
-    void keyboardInput();
-    void stopServer();
+    Server(const char *_ip, int _port);//생성자
+    void startServer();//서버시작 메서드
+    void runServer();//서버 동작중 메서드
+    void receiving();//서버에서 데이터 보내는 메서드
+    void sending();//클라이언트에서 데이터 받는 메서드
+    void keyboardInput();//키보드에서 입력 받는 함수
+    void stopServer();// 서버 종료 메서드
 };
